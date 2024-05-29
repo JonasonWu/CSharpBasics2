0f02592d4fc8ac47fbb416729465bfdf7b6bb1ae
JonasonWu <jw5911@nyu.edu>
2024-05-29 17:01:45 -0400

    Add project files.

---
+++ PracticingArrays/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PracticingArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
